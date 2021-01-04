eaff0bc415c4795d65f171eb0322c9de69b17503
rashmibaptay <rashmibaptay@gmail.com>
2021-01-04 20:45:59 +0300

    first ever Commit

---
+++ src/test/eg1.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class eg1 {
+@Test
+public void demo()
+{
+	System.out.println("hello TestNG");
+}
+@Test
+public void demo2()
+{
+System.out.println("Bye");
+}
+}
